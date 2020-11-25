53bb165f87bb5752c672ee13f783a8b24aa91d55
AndresGallop <andresgallod.i@gmail.com>
2020-11-25 18:21:06 -0500

    lightbox added

--- js/modules/DataMiner.js
+++ js/modules/DataMiner.js
@@ -1,9 +1,15 @@
 async function fetchData(datasource){
     let resource = await fetch(datasource).then(response => {
         // bang operator - means "does not equal" (or a falsy value)
+        
         if (response.status !== 200) {
+
+            alert(`Hey my friend, you have a ${response.status} Error`);
             throw new Error(`Danger Will Robinson! Here there be monsters! Error ${response.status}`);
+            
         }
+
+      
 
         return response;
     })
